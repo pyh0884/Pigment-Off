6a62e90d398b2aef10f34a27f4221a456821ed5d
pyh0884 <pyh2084@hotmail.com>
2020-01-02 23:18:01 +0800

    111

---
+++ Assets/Scripts/Sniper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sniper : MonoBehaviour
+{
+    public GameObject EmitPoint;
+    private LineRenderer laser;
+    void Start()
+    {
+        laser = GetComponent<LineRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        laser.SetPosition(0, EmitPoint.transform.position);
+    }
+}
