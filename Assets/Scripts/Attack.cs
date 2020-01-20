9efc008a2ee41aebe3c98e0a2c5f3d65aa3efd25
pyh0884 <pyh2084@hotmail.com>
2020-01-20 21:17:15 +0800

    1

--- Assets/Scripts/Attack.cs
+++ Assets/Scripts/Attack.cs
@@ -26,8 +26,9 @@ public class Attack : MonoBehaviour
     //Cannon
     [Header("重炮角色：")]
     public bool isCannon;
-    public LayerMask CannonBlockRay;
     public LineRenderer TrajectoryLine;
+    private bool CannonAiming = false;
+    private bool CannonPressed = false;
     //--------------------------------
     //Basic Attack Parameters
     [Header("基本参数：")]
@@ -49,10 +50,10 @@ public class Attack : MonoBehaviour
     public float TargetMp = 100;
     public float lerpSpeed = 5;
     [Header("颜料增长速度")]
-    public float MpIncreaseSpeed = 5;    
+    public float MpIncreaseSpeed = 5;
     [Header("回复速度提高倍率")]
     public bool MpBoost = false;
-    public float MpBoostNum = 3;    
+    public float MpBoostNum = 3;
     [Header("回复速度降低倍率")]
     public bool MpSlow = false;
     public float MpSlowNum = 3;
@@ -77,13 +78,13 @@ public class Attack : MonoBehaviour
         tempCD = CdTime;
         anim = main.GetComponent<Animator>();
     }
-    public void Shoot()
+    public void Shoot() //普通型攻击
     {
         var bul = Instantiate(Bullet, EmitPoint.transform.position, Quaternion.identity);
-        if (Mathf.Sqrt(Mathf.Pow(Camera.main.ScreenToWorldPoint(Input.mousePosition).x-transform.position.x,2)+ Mathf.Pow(Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y,2)) < 12) //Todo：增加射程变量
+        if (Mathf.Sqrt(Mathf.Pow(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x, 2) + Mathf.Pow(Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y, 2)) < 12) //Todo：增加射程变量
         {
             //Debug.Log(Mathf.Sqrt(Mathf.Pow(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x, 2) + Mathf.Pow(Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y, 2)));
-            bul.GetComponent<BulletAI>().Range= Mathf.Sqrt(Mathf.Pow(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x, 2) + Mathf.Pow(Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y, 2));
+            bul.GetComponent<BulletAI>().Range = Mathf.Sqrt(Mathf.Pow(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x, 2) + Mathf.Pow(Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y, 2));
         }
         bul.GetComponent<Rigidbody2D>().velocity = new Vector2(dir.x, dir.y).normalized * bul.GetComponent<BulletAI>().ShootSpeed;
         if (angle >= -90 && angle <= 90)
@@ -149,18 +150,84 @@ public class Attack : MonoBehaviour
         LeftPoint = Vector3.Lerp(LeftPoint, EndPoint, AimingSpeed * Time.deltaTime);
         RightPoint = Vector3.Lerp(RightPoint, EndPoint, AimingSpeed * Time.deltaTime);
     }
+    public void SniperShoot()
+    {
+        StartAiming = false;
+        laserLeft.enabled = false;
+        laserRight.enabled = false;
+        pressed = false;
+        anim.SetTrigger("Attack"); //不同的Animatioin,动画事件使用新Method
+        RandomDir = (EmitPoint.transform.position + LeftPoint + (RightPoint - LeftPoint).normalized * Random.Range(0, (RightPoint - LeftPoint).magnitude)) - EmitPoint.transform.position;
+        var bul = Instantiate(Bullet, EmitPoint.transform.position, Quaternion.identity);
+        bul.GetComponent<Rigidbody2D>().velocity = new Vector2(RandomDir.x, RandomDir.y).normalized * bul.GetComponent<BulletAI>().ShootSpeed;
+        bul.transform.rotation = Quaternion.AngleAxis(Mathf.Atan2(RandomDir.y, RandomDir.x) * Mathf.Rad2Deg, Vector3.forward);
+        cdTime = 0;
+    }
+    private float VelocityZ = 1 / 1.2f + 10 * 1.2f;
+    private float VelocityY;
+    private float VelocityX;
+    public void CannonTrajectory()
+    {
+        TrajectoryLine.enabled = true;
+        TrajectoryLine.SetPosition(0, EmitPoint.transform.position);
+        VelocityX = (Camera.main.ScreenToWorldPoint(Input.mousePosition).x - EmitPoint.transform.position.x) / 1.2f;
+        VelocityY = (Camera.main.ScreenToWorldPoint(Input.mousePosition).y - EmitPoint.transform.position.y) / 1.2f;
+        VelocityZ = 1 / 1.2f + 10 * 1.2f;
+        float i = 0.02f;
+        while (i < 1 && (Mathf.Abs(EmitPoint.transform.position.x + VelocityX * i * 1.75f - Camera.main.ScreenToWorldPoint(Input.mousePosition).x) > 0.2f || i < 0.65f)) 
+        {
+            TrajectoryLine.positionCount = Mathf.RoundToInt(i / 0.02f) + 1;
+            TrajectoryLine.SetPosition(Mathf.RoundToInt(i / 0.02f), new Vector3(EmitPoint.transform.position.x + VelocityX * i*1.75f, EmitPoint.transform.position.y + (VelocityY * i + VelocityZ * i) * 1.95f,0));
+            VelocityZ -= 20 * 0.02f;
+            i += 0.02f;
+        }
+        //for (float i = 0.02f; i < 1.5f; i += 0.02f)
+        //{
+        //    TrajectoryLine.positionCount = Mathf.RoundToInt(i / 0.02f) + 1;
+        //    TrajectoryLine.SetPosition(Mathf.RoundToInt(i / 0.02f), new Vector3(EmitPoint.transform.position.x + VelocityX * i * 1.75f, EmitPoint.transform.position.y + (VelocityY * i + VelocityZ * i) * 1.95f));
+        //    VelocityZ -= 20 * 0.02f;
+        //}
+    }
+    public void CannonShoot()
+    {
+        CannonAiming = false;
+        TrajectoryLine.enabled = false;
+        var bul = Instantiate(Bullet, EmitPoint.transform.position, Quaternion.identity);
+        bul.GetComponent<CannonBullet>().StartShoot(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+    }
     void Update()
     {
         cdTime += Time.deltaTime;
         dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
         angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
         GunSprite.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-        //Sniper
+        //狙击射击（松开左键时）
+        if ((isSniper && Input.GetKeyUp(KeyCode.Mouse0) && pressed) /*|| (Mp <= 5 && isSniper && pressed)*/)
+        {
+            SniperShoot();
+        }
+        //狙击按下左键时瞄准
         if (isSniper && Input.GetKey(KeyCode.Mouse0) && Mp >= MpCost && CanAttack && cdTime > CdTime)
         {
             StartAiming = true;
         }
-        //--------------------------------------------------------
+        if (StartAiming)
+        {
+            SniperAiming();
+        }
+        //Cannon按下左键蓄力？？？？？？
+        if (isCannon && Input.GetKey(KeyCode.Mouse0) && Mp >= MpCost && CanAttack && cdTime > CdTime)
+        {
+            CannonPressed = true;
+            CannonAiming = true;
+        }
+
+        //Cannon松开左键发射
+        if ((isCannon && Input.GetKeyUp(KeyCode.Mouse0) && CannonPressed) /*|| (Mp <= 5 && isSniper && pressed)*/)
+        {
+            CannonShoot();
+        }
+        //普通型射击（按下/按住左键）
         if (!isSniper && Input.GetKey(KeyCode.Mouse0) && Mp >= MpCost && CanAttack && cdTime > CdTime)
         {
             anim.SetBool("Attack", true);
@@ -169,25 +236,6 @@ public class Attack : MonoBehaviour
         {
             anim.SetBool("Attack", false);
         }
-        if (StartAiming)
-        {
-            SniperAiming();
-        }
-        //狙击射击（松开左键时）
-        if ((isSniper && Input.GetKeyUp(KeyCode.Mouse0) && pressed) /*|| (Mp <= 5 && isSniper && pressed)*/)
-        {
-            StartAiming = false;
-            laserLeft.enabled = false;
-            laserRight.enabled = false;
-            pressed = false;
-            anim.SetTrigger("Attack"); //不同的Animatioin,动画事件使用新Method
-            RandomDir = (EmitPoint.transform.position+LeftPoint+(RightPoint - LeftPoint).normalized * Random.Range(0, (RightPoint - LeftPoint).magnitude))-EmitPoint.transform.position;
-            var bul = Instantiate(Bullet, EmitPoint.transform.position, Quaternion.identity);
-            bul.GetComponent<Rigidbody2D>().velocity= new Vector2(RandomDir.x, RandomDir.y).normalized * bul.GetComponent<BulletAI>().ShootSpeed;
-            bul.transform.rotation= Quaternion.AngleAxis(Mathf.Atan2(RandomDir.y, RandomDir.x) * Mathf.Rad2Deg, Vector3.forward);
-            cdTime = 0;
-        }
-
         if (Mp < MpMax)
         {
             if (!MpBoost && !MpSlow)
@@ -210,6 +258,13 @@ public class Attack : MonoBehaviour
             Mp = Mathf.Lerp(Mp, TargetMp, Time.deltaTime * lerpSpeed);
         }
         slider.value = (float)(Mp / MpMax);
+    }
+    private void FixedUpdate()
+    {
+        if (CannonAiming)
+        {
+            CannonTrajectory();
+        }
     }
 
 }
---
+++ Assets/Scripts/CannonBullet.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBullet : MonoBehaviour
+{
+    public Vector3 targetPos;
+    public float xOffSet;
+    public float yOffSet;
+    public float velocity = 20;//炮弹速度    
+    public float velocityX;
+    public float velocityY;
+    public float velocityZ0;
+    private float time = 1;
+    private float screenY;
+    private float screenZ;
+    private float screenX;
+    private bool start = false;
+    private float timer = 0;
+    #region same height
+    public float height = 1;
+    public float TotalTime = 1.2f;
+
+    public void StartShoot(Vector3 target)
+    {
+        start = true;
+        targetPos = target;
+        velocityX = (Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x) / TotalTime;
+        velocityY = (Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y) / TotalTime;
+        velocityZ0 = height / TotalTime + 10 * TotalTime;
+    }
+    void Start()
+    {
+        screenX = transform.position.x;
+        screenY = transform.position.y;
+        screenZ = 0;
+    }
+    private void FixedUpdate()
+    {
+        timer += Time.fixedDeltaTime;
+        if (timer > TotalTime)
+        {
+            Destroy(gameObject);
+            //Todo:Explsion
+        }
+        if (start)
+        {
+            screenX += velocityX * Time.fixedDeltaTime;
+            screenY += velocityY * Time.fixedDeltaTime;
+            velocityZ0 -= 20 * Time.fixedDeltaTime;
+            screenZ -= velocityZ0 * Time.fixedDeltaTime;
+            //transform.position = new Vector3(screenX, screenY, screenZ);
+            transform.position = new Vector3((screenX - xOffSet), (screenY - screenZ - yOffSet),0);
+        }
+
+    }
+    #endregion
+    //#region same velocity
+    //public void StartShoot(Vector3 target)
+    //{
+    //    start = true;
+    //    targetPos = target;
+    //    time = Mathf.Sqrt(Mathf.Pow(Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x, 2) + Mathf.Pow(Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y, 2)) / velocity;
+    //    velocityX = (Camera.main.ScreenToWorldPoint(Input.mousePosition).x - transform.position.x) / time;
+    //    velocityY = (Camera.main.ScreenToWorldPoint(Input.mousePosition).y - transform.position.y) / time;
+    //    velocityZ0 = -20 / 2 * time;
+    //}
+    //void Start()
+    //{
+    //    screenX = transform.position.x;
+    //    screenY = transform.position.y;
+    //    screenZ = 0;
+    //}
+    //private void FixedUpdate()
+    //{
+    //    timer += Time.fixedDeltaTime;
+    //    if (timer > time)
+    //    {
+    //        Destroy(gameObject);
+    //        //Todo:Explsion
+    //    }
+    //    if (start)
+    //    {
+    //        screenX += velocityX * Time.fixedDeltaTime;
+    //        screenY += velocityY * Time.fixedDeltaTime;
+    //        velocityZ0 += 20 * Time.fixedDeltaTime;
+    //        screenZ += velocityZ0 * Time.fixedDeltaTime;
+    //        //transform.position = new Vector3(screenX, screenY, screenZ);
+    //        transform.position = new Vector3((screenX - xOffSet), (screenY - screenZ - yOffSet), 0);
+    //    }
+
+    //}
+    //#endregion
+}
