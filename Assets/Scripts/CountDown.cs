56d83f511d44a6c886ec14f80369afdf5d41ba57
pyh0884 <pyh2084@hotmail.com>
2020-05-05 03:47:31 +0800

    1

---
+++ Assets/Scripts/CountDown.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountDown : MonoBehaviour
+{
+    GameManager gm;
+    Image img;
+    void Start()
+    {
+        gm = FindObjectOfType<GameManager>();
+        img = GetComponent<Image>();
+    }
+
+    void Update()
+    {
+        img.fillAmount = gm.PlayTime / 300f;
+    }
+}
